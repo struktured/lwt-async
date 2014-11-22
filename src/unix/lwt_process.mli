cad468c37cf44436c545763e3bd0f2b5d963d357
carm <carmelo.piccione@gmail.com>
2014-11-21 21:12:08 -0500

    54c971716c4af0882c98ef33685f0ca4d7236277 to a1f62975b56399dabaf0affc1876e7d8e2afd781 from lwt github

--- src/unix/lwt_process.mli
+++ src/unix/lwt_process.mli
@@ -1,5 +1,5 @@
 (* Lightweight thread library for OCaml
- * http://www.output_channelsigen.org/lwt
+ * http://www.ocsigen.org/lwt
  * Module Lwt_process
  * Copyright (C) 2009 Jérémie Dimino
  *
@@ -22,7 +22,7 @@
 
 (** Process management *)
 
-(** This modules allow you to spawn processes and communicate with them. *)
+(** This module allows you to spawn processes and communicate with them. *)
 
 type command = string * string array
     (** A command. The first field is the name of the executable and
@@ -59,15 +59,15 @@ val shell : string -> command
   (** A command executed with the shell. (with ["/bin/sh -c <cmd>"] on
       Unix and ["cmd.exe /c <cmd>"] on Windows). *)
 
-(** All the following functions take an optionnal argument
+(** All the following functions take an optional argument
     [timeout]. If specified, after expiration, the process will be
     sent a [Unix.sigkill] signal and channels will be closed. *)
 
-(** {6 High-level functions} *)
+(** {2 High-level functions} *)
 
-(** {8 Redirections} *)
+(** {3 Redirections} *)
 
-(** A file descriptor redirection. It describe how standard file
+(** A file descriptor redirection. It describes how standard file
     descriptors are redirected in the child process. *)
 type redirection =
     [ `Keep
@@ -83,9 +83,9 @@ type redirection =
         (** The file descriptor is replaced by the given one, which is
             then closed. *) ]
 
-(** Note: all optionnal redirection argumetns default to [`Keep] *)
+(** Note: all optional redirection arguments default to [`Keep] *)
 
-(** {8 Executing} *)
+(** {3 Executing} *)
 
 val exec :
   ?timeout : float ->
@@ -96,7 +96,7 @@ val exec :
   command -> Unix.process_status Lwt.t
   (** Executes the given command and returns its exit status. *)
 
-(** {8 Receiving} *)
+(** {3 Receiving} *)
 
 val pread :
   ?timeout : float ->
@@ -123,7 +123,7 @@ val pread_lines :
   ?stderr : redirection ->
   command -> string Lwt_stream.t
 
-(** {8 Sending} *)
+(** {3 Sending} *)
 
 val pwrite :
   ?timeout : float ->
@@ -150,7 +150,7 @@ val pwrite_lines :
   ?stderr : redirection ->
   command -> string Lwt_stream.t -> unit Lwt.t
 
-(** {8 Mapping} *)
+(** {3 Mapping} *)
 
 val pmap :
   ?timeout : float ->
@@ -173,7 +173,7 @@ val pmap_lines :
   ?stderr : redirection ->
   command -> string Lwt_stream.t -> string Lwt_stream.t
 
-(** {6 Spawning processes} *)
+(** {2 Spawning processes} *)
 
 (** State of a sub-process *)
 type state =
@@ -202,7 +202,7 @@ object
 
   method terminate : unit
     (** Terminates the process. It is equivalent to [kill Sys.sigkill]
-        on Unix but also works on windows (unlike {!kill}). *)
+        on Unix but also works on Windows (unlike {!kill}). *)
 
   method status : Unix.process_status Lwt.t
     (** Threads which wait for the sub-process to exit then returns its
@@ -213,7 +213,7 @@ object
         its resource usages *)
 
   method close : Unix.process_status Lwt.t
-    (** Closes the process and returns its exit status. This close all
+    (** Closes the process and returns its exit status. This closes all
         channels used to communicate with the process *)
 end
 
